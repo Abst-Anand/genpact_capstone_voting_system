a170c29ca0f9099a7373f8020fa8202c33fc6bde
Anand Raj <cse.20bcse33@silicon.ac.in>
2024-10-04 15:50:36 +0530

    Changed iframe source of subAdmin Dashboard

--- src/main/java/com/votingSystem/repository/CandidateRepository.java
+++ src/main/java/com/votingSystem/repository/CandidateRepository.java
@@ -2,7 +2,6 @@ package com.votingSystem.repository;
 
 import com.votingSystem.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
